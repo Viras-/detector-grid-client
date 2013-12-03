284ea47d467b3a8c61019a182ef7d1d3150a9e3d
Viras- <viras@users.sourceforge.net>
2013-12-03 18:14:12 +0100

    moved message handling from listener to client implementation

--- src/at/technikum_wien/detectorgridclient/communication/Client.java
+++ src/at/technikum_wien/detectorgridclient/communication/Client.java
@@ -17,16 +17,66 @@
 package at.technikum_wien.detectorgridclient.communication;
 
 import at.technikum_wien.detectorgridclient.TagInformation;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
  * @author wkoller
  */
-public interface Client {
+public abstract class Client {
     public abstract boolean init(String address);
     
-    public abstract boolean addListener(Listener listener);
-    public abstract boolean removeListener(Listener listener);
+    protected ArrayList<Listener> listeners = new ArrayList<>();
+    
+    public boolean addListener(Listener listener) {
+        listeners.add(listener);
+        
+        return true;
+    }
+    
+    public boolean removeListener(Listener listener) {
+        listeners.remove(listener);
+        
+        return true;
+    }
+
+    /**
+     * Handle an incoming message and trigger the action for it
+     *
+     * @param messageComponents
+     * @return
+     * @throws Exception
+     */
+    public void handleMessage(String[] messageComponents) throws Exception {
+        // check if we have at least one message component
+        if(messageComponents.length <= 0) {
+            throw new Exception("Invalid message components passed to 'handleMessage'");
+        }
+        
+        // determine action and trigger function for it
+        switch (messageComponents[0]) {
+            case "findTag":
+                // check for tag-code to find in message
+                if (messageComponents.length >= 2) {
+                    Iterator<Listener> l_it = listeners.iterator();
+                    while( l_it.hasNext() ) {
+                        Listener currListener = l_it.next();
+                        
+                        // ask listener for tag information
+                        TagInformation tagInformation = currListener.findTag(messageComponents[1]);
+                        if( tagInformation != null ) {
+                            this.foundTag(tagInformation);
+                        }
+                    }
+                } else {
+                    throw new Exception("Invalid Message: '" + messageComponents[0] + "' - missing payload!");
+                }
+            // all other cases are invalid messages
+            default:
+                throw new Exception("Unknown Message: '" + messageComponents[0] + "'");
+        }
+    }
 
     /**
      * communicate a found tag to the server over this client
